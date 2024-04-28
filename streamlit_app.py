a253a9556135be9be7e3e9558070fc261edc2bed
naboth254 <101576712+naboth254@users.noreply.github.com>
2024-04-28 14:17:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from datetime import datetime
+import requests
 
 def get_time_of_day():
     current_hour = datetime.now().hour
@@ -26,6 +26,15 @@ def get_greeting(language):
     }
     return greetings.get(language, greetings['english']).get(time_of_day)
 
+def get_quote_of_the_day():
+    url = "https://favqs.com/api/qotd"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data['quote']['body']
+    else:
+        return "Unable to fetch the quote of the day"
+
 def main():
     st.title('Hello App')
     name = st.text_input('Enter your name:')
@@ -38,6 +47,10 @@ def main():
             st.write(f'{greeting}, {name}!')
         else:
             st.write(f'{greeting}!')
+
+    st.write('Quote of the day:')
+    quote = get_quote_of_the_day()
+    st.write(quote)
 
     st.write('Current date and time:', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
